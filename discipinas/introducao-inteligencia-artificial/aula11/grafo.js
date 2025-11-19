8496966890766761d64de62dee6ee0494451e087
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2025-11-19 12:32:08 +0000

    Renaming function best_choice to dfs and updating variable names for clarity

--- discipinas/introducao-inteligencia-artificial/aula11/grafo.js
+++ discipinas/introducao-inteligencia-artificial/aula11/grafo.js
@@ -1,4 +1,4 @@
-const best_choice = (graph = {}, start = '', goal = '') => {
+const dfs = (graph = {}, start = '', goal = '') => {
   const steps = [[start]]
 
   while (steps.at(steps.length - 1).at(steps.at(steps.length - 1).length - 1) != goal) {
@@ -12,16 +12,18 @@ const best_choice = (graph = {}, start = '', goal = '') => {
   }
 
   return steps.find((step) => {
-    return step.at(0) == start && step.at(step.length - 1) == goal
+    const start_ok = step.at(0) == start
+    const goal_ok  = step.at(step.length - 1) == goal
+    return start_ok && goal_ok
   })
 }
 
-const cidades = {
+const cities = {
   "Campinas": ["Jundiaí", "Indaiatuba"],
   "Jundiaí": ["São Paulo"],
   "Indaiatuba": ["São Paulo"],
   "São Paulo": []
 }
 
-const caminho = best_choice(cidades, "Campinas", "São Paulo")
-console.log(caminho)
+const flow = dfs(cities, "Campinas", "São Paulo")
+console.log(flow)
