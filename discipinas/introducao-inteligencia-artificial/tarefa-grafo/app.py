f90356d683bccc30e99da2a93b8bb749d10fb829
Tarsis Lima <br.tmvdl@gmail.com>
2025-12-12 09:20:15 -0300

    Update files

--- discipinas/introducao-inteligencia-artificial/tarefa-grafo/app.py
+++ discipinas/introducao-inteligencia-artificial/tarefa-grafo/app.py
@@ -5,3 +5,35 @@ grafo = [
   [1, 0, 0, 0, 1], # D
   [0, 0, 0, 1, 0], # E
 ]
+
+# from C to E
+
+initial_position = 'C'
+final_position = 'E'
+
+# way = ['C', 'B', 'A', 'D', 'E']
+
+signs = { 'A' : 0, 'B' : 1, 'C' : 2, 'D' : 3, 'E' : 4 }
+
+letters = ['A', 'B', 'C', 'D', 'E']
+
+def walk(walking): # ['C']
+  steps = [walking]
+  walking_ix = [signs[x] for x in walking]
+  index = walking_ix[-1] # ix = 2
+  point = grafo[index] # [0, 1, 0, 0, 0]
+
+  for conn in point:
+    ix = point.index(conn)
+    if letters[ix] in walking:
+      continue
+
+    if conn == 1: # 0 = not a connection, 1 = a connection
+      step = []
+      step.extend(walking)
+      step.append(letters[ix])
+      steps.append(step)
+
+  return steps # [['C', 'B']]
+
+print(walk(['C', 'B']))
