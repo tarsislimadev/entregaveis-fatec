92bc6df4a2e8536513caf60224481d1514206c78
Tarsis Lima <br.tmvdl@gmail.com>
2026-01-20 11:06:10 -0300

    Update files

---
+++ discipinas/introducao-inteligencia-artificial/aula-2026-01-20/largura.js
@@ -0,0 +1,39 @@
+const WIDTH = 5
+
+const map = [
+  [0, 1, 1, 1, 1],
+  [0, 0, 0, 0, 1],
+  [1, 1, 1, 0, 1],
+  [0, 0, 1, 0, 1],
+  [0, 0, 1, 0, 0],
+]
+
+const begin = [0, 0]
+const end = [4, 4]
+
+const step = (here, there, map, steps = []) => {
+  const [x_here, y_here] = here
+  const [x_there, y_there] = there
+
+  // is it final?
+  if (x_here == x_there && y_here == y_there) {
+    return steps
+  }
+
+  // find neighbors
+  const neighbors = [
+    [x_here - 1, y_here], // up
+    [x_here + 1, y_here], // down
+    [x_here, y_here - 1], // left
+    [x_here, y_here + 1], // right
+  ].filter(([x, y]) => map[x][y] == 0)
+
+  neighbors.map((neighbor) => {
+    const [x, y] = neighbor
+  })
+
+  return null
+}
+
+const way = step(begin, end, map, [begin])
+console.log({ way })
